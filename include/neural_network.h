9973ef09e905b82963948b62d68f71915a8fef6b
aresaguilar <aresaguilarsotos@gmail.com>
2016-02-13 14:56:49 +0100

    Constructors created

--- include/neural_network.h
+++ include/neural_network.h
@@ -20,28 +20,25 @@ typedef struct Neural_Layer_ {
 } Neural_Layer;
 
 typedef struct Neural_Network_ {
+    int n_neurons;                 /* Total number of neurons in the net */
     int n_layers;                  /* Number of layers in the network (including in/out) */
     Neural_Layer * layers;         /* Layers array */
 } Neural_Network;
 
 
 
-
-
-
-
-
 /* Public Methods */
-/* Allocates memory for the neural network */
-Neural_Network * nn_new();
+/* Allocates memory for the neural network with n layers and
+   n_neurons_layer per layer (includind first/last layers) */
+Neural_Network * nn_new(int n_layers, int * n_neurons_layer);
 
 /* Reads a neural network from a file.
    Supposing we have the following network with 3 layers:
-   o
+       o
    o <   \
-   o - o
+       o - o
    o <   /
-   o
+       o
    Assuming all weights equal to 1, the file to represent it should be as follows:
    2 3 1                       # Number of perceptrons for each layer
    0.0 0.0 1.0 1.0 0.0 0.0     # First perceptron connections (with weights)
@@ -59,12 +56,22 @@ int nn_read_from_stdin(Neural_Network * nn);
 
 
 /* Private Methods */
+/* Neural Network Methods */
+/* Access the network array of neurons */
+/* TODO: Not sure this works */
+#define nn_array(nn) (nn)->layers[0].neurons
+
+/* Sets the connections to a neuron from the others */
+int nn_connect_neuron(Neural_Network * nn, int n_neuron_to, double * w_neurons_from);
+
+/* Neural_Layer methods */
+/* Sets a previously allocated layer variables (number of neurons and array of neurons) */
+int layer_init(Neural_Layer * l, int n_neurons, Neuron * first);
 
 
-
-
-
-
+/* Neuron methods */
+/* Sets the default values for a neuron */
+int neuron_init(Neuron * n);
 
 
 #endif /* NEURAL_NETWORK_H */
--- src/neural_network.c
+++ src/neural_network.c
@@ -3,11 +3,107 @@
 #include "neural_network.h"
 
 /* Public Methods */
-Neural_Network * nn_new() {
-  Neural_Network * nn = (Neural_Network *) malloc(sizeof(Neural_Network));
-  if (!nn) return NULL;
-  nn->n_layers = 0;
-//  nn->n_perceptrons = 0;
-  nn->layers = NULL;
-  return nn;
+Neural_Network * nn_new(int n_layers, int * n_nerons_layer) {
+    Neural_Network * nn;
+    Neuron * neurons_arr;
+    int n_neurons_total;
+    int i, offset;
+
+    /* Compute the total number of neurons */
+    n_neurons_total = 0;
+    for (i = 0; i < n_layers; i++) {
+        n_neurons_total += n_neurons_layer[i];
+    }
+
+    /* Allocates memory for the neurons */
+    neurons_arr = (Neuron *) malloc(n_neurons_total * sizeof(Neuron));
+    if (!neurons_arr) return NULL;
+    /* Initialize neurons */
+    for (i = 0; i < n_neurons_total; i++) {
+        neuron_init(neurons_arr + i);
+    }
+    
+    /* Allocates memory for the network */
+    nn = (Neural_Network *) malloc(sizeof(Neural_Network));
+    if (!nn) {
+        free(neurons_arr);
+        return NULL;
+    }
+
+    /* Allocates memory for the layers */
+    nn->layers = (Neural_Layer *) malloc(n_layers * sizeof(Neural_Layer));
+    if (!nn->layers) {
+        free(nn);
+        free(neurons_arr);
+        return NULL;
+    }
+
+    /* Set the network properties */
+    nn->n_neurons = n_neurons_total;
+    nn->n_layers = n_layers;
+    offset = 0;
+    for (i = 0; i < n_layers; i++) {
+        /* TODO: Check pointers */
+        layer_init(&(nn->layers[i]), n_neurons_layer[i], neurons_arr + offset);
+        offset += n_neurons_layer[i];
+    }
+    
+    return nn;
+}
+
+
+
+/* Private Methods */
+/* Neural Network Methods */
+int nn_connect_neuron(Neural_Network * nn, int n_neuron_to, double * w_neurons_from) {
+    Neuron * n_to;
+    int n_connections;
+    int i;
+    
+    if (!nn | !w_neurons_from) return -1;
+
+    /* Get total number of connections */
+    n_connections = 0;
+    for (i = 0; i < nn->n_neurons; i++) {
+        if (w_neurons_from[i] != 0.0) {
+            n_connections++;
+        }
+    }
+    if (n_connections == 0) return 0;
+
+    /* Allocate memory for the connections */
+    n_to = &(nn_array(nn)[n_neuron_to]);
+    n_to->cons = (Connection *) malloc(n_connections * sizeof(Connection));
+    if (!(n_to->cons)) return -1;
+
+    /* Stablish the connections */
+    n_connections = 0;
+    for (i = 0; i < nn->n_neurons; i++) {
+        if (w_neurons_from[i] != 0.0) {
+            n_to->cons[n_connections].weight = w_neurons_from[i];
+            n_to->cons[n_connections].from = &(nn_array(nn)[i]);
+            n_connections++;
+        }
+    }
+
+    return 1;
+}
+
+
+/* Layer Methods */
+int layer_init(Neural_Layer * l, int n_neurons, Neuron * first) {
+    if (!l) return -1;
+    l->n_neurons = n_neurons;
+    l->neurons = first;
+    return 0;
+}
+
+/* Neuron Methods */
+int neuron_init(Neuron * n) {
+    if (!n) return -1;
+    n->d = 0.0;
+    n->err = 0.0;
+    n->n_cons = 0;
+    n->cons = NULL;
+    return 0;
 }
