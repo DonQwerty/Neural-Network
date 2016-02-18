f78950fa9c9b314f8e94db50ef2a3e81a4832c15
daniel <gimenez767@gmail.com>
2016-02-18 20:31:52 +0100

    Ej 1

--- include/neural_network_functions.h
+++ include/neural_network_functions.h
@@ -4,6 +4,8 @@
 #include "neural_network.h"
 
 void upd_neuron_mcculloch_pitts (Neuron * neuron);
+void upd_weights_perceptron (Neuron * neuron,double alpha, double t);
+void upd_weights_adeline (Neuron * neuron,double alpha, double t);
 
 
 #endif /* NEURAL_NETWORK_FUNCTIONS_H */
--- src/neural_network_functions.c
+++ src/neural_network_functions.c
@@ -24,21 +24,19 @@ void upd_neuron_mcculloch_pitts (Neuron * neuron){
 }
 
 
-void upd_weights_perceptron (Neuron * neuron){
+void upd_weights_perceptron (Neuron * neuron,double alpha, double t){
 	int i;
-	double value = 0;
-	double weight;
 	for ( i = 0; i < neuron->n_cons; i++){
-		weight = neuron->cons[i].weight;
-		if(weight < 0){
-			value = 0;
-			break;
-		}
-		value += weight * neuron->cons[i].from->d;
+		if(neuron->d != t)
+			neuron->cons[i].weight = neuron->cons[i].weight + alpha * t * neuron->cons[i].from->d;
 	}
-	if(value < neuron->threshold)
-		neuron->d_new = 0;
-	else
-		neuron->d_new = 1;
+	return;
+}
+
+void upd_weights_adeline (Neuron * neuron,double alpha, double t){
+	int i;
+	for ( i = 0; i < neuron->n_cons; i++){
+			neuron->cons[i].weight = neuron->cons[i].weight + alpha * (t - neuron->d) * neuron->cons[i].from->d;
+	}
 	return;
 }
